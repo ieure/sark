ac110853c7787157fa0583ddd508e64afde7841e
Ian Eure <ian.eure@gmail.com>
2013-09-01 21:33:08 -0700

    Bump version to alpha 2.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sark "0.1.0-SNAPSHOT"
+(defproject sark "alpha 2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
