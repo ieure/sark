29b3a589604461771577ee2d65734f23899d88b3
Ian Eure <ian.eure@gmail.com>
2013-09-02 08:40:51 -0700

    Alpha 3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sark "alpha 2"
+(defproject sark "alpha3-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
