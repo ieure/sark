6785f08a51eff035b8b472173e9c1adb492751ca
Ian Eure <ian.eure@gmail.com>
2013-09-02 08:52:27 -0700

    docs etc.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject sark "alpha3-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :description "Sark, the searchable Arcade Archive"
+  :url "http://github.com/ieure/sark"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
