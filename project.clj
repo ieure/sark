cef0aadb4b01545d771e956a9960b3ebe57322c3
Ian Eure <ian.eure@gmail.com>
2014-12-19 22:27:23 -0800

    Refresh deps

--- project.clj
+++ project.clj
@@ -4,17 +4,19 @@
   :url "http://github.com/ieure/sark"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [clj-http "0.7.6"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [clj-http "1.0.1"]
                  [clucy "0.4.2-SNAPSHOT"]
-                 [compojure "1.1.5"]
-                 [ring-middleware-format "0.3.1"]
-                 [ring/ring-jetty-adapter "1.2.0"]]
+                 [compojure "1.3.1"]
+                 [ring-middleware-format "0.4.0"]
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [metrics-clojure "2.4.0"]]
   :aot :all
-  :plugins [[lein-ring "0.8.6"]]
+  :plugins [[lein-ring "0.8.13"]]
   :jvm-opts ["-Djava.awt.headless=true"] ; Fuk u osx
   :ring {:handler sark.api/handler
+         :war-resources "resources"
          :init sark.core/init
          :auto-reload? true}
   :main sark.cli)
