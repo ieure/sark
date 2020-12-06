07e819211a724f73b260f838d59c70ef566e2026
Ian Eure <ian@retrospec.tv>
2020-12-05 17:14:50 -0800

    Update deps

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :url "http://github.com/ieure/sark"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.taoensso/timbre "3.3.1"]
-                 [clj-http "1.0.1"]
-                 [clucy "0.4.2-SNAPSHOT"]
-                 [compojure "1.3.1"]
-                 [ring-middleware-format "0.4.0"]
-                 [ring/ring-jetty-adapter "1.3.2"]
-                 [metrics-clojure "2.4.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.taoensso/timbre "5.1.0"]
+                 [clj-http "3.11.0"]
+                 [clucy "0.4.0"]
+                 [compojure "1.6.2"]
+                 [ring-middleware-format "0.7.4"]
+                 [ring/ring-jetty-adapter "1.8.2"]
+                 [metrics-clojure "2.10.0"]]
   :aot :all
-  :plugins [[lein-ring "0.8.13"]]
   :jvm-opts ["-Djava.awt.headless=true"] ; Fuk u osx
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler sark.api/handler
          :war-resources "resources"
          :init sark.core/init
